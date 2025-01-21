8fc38f5f7399fa4079a387abe628480b0fb52965
src_resources <src.res.211@gmail.com>
2025-01-22 00:08:59 +0800

    Initialise the repository

---
+++ src/main/kotlin/top/srcres258/tutorialmod/TutorialModClient.kt
@@ -0,0 +1,9 @@
+package top.srcres258.tutorialmod
+
+import net.fabricmc.api.ClientModInitializer
+
+object TutorialModClient : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
